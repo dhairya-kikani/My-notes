a13b81744263f84f333bdffb359a84fa1ce98172
dhairya-kikani <kikanidhairya@gmail.com>
2023-02-23 19:29:51 +0530

    step-11

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,5 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
 import 'package:mynotes/services/auth/firebase_auth_provider.dart';
---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenExeption implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception{}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteuser implements Exception{}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
